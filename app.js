e2ff2178505b55cd4baa2cd7f67062d59bc903d1
Rubiksron <rubiksron@gmail.com>
2017-06-24 14:22:11 -0700

    weather api testing.

--- app.js
+++ app.js
@@ -10,23 +10,28 @@ var view = (function view() {/*
     </html>
 */}).toString().match(/[^]*\/\*([^]*)\*\/\s*\}$/)[1];
 
-module.exports = function(ctx, cb) {
-  cb(null, { hire: ctx.data.name || 'Ron Dunphy' });
-};
 
 var request = require('request');
 
 module.exports =
     function (cb) {
         var start = Date.now();
-        request.get('http://api.wunderground.com/api/Your_Key/conditions/q/CA/Seattle.json', function (error, res, body) {
+        request.get('http://api.wunderground.com/api/cc8285fae724e71a/conditions/q/CA/San_Francisco.json', function (error, res, body) {
             if (error)
                 cb(error);
             else
                 cb(null, {
                     status: res.statusCode,
                     length: body.length,
-                    latency: Date.now() - start
+                    latency: Date.now() - start,
+                    weather:  [
+                      {
+                        id: 800,
+                        main: "Clear",
+                        description: "clear sky",
+                        icon: "02n"
+                      }
+                    ]
                 });
         });
     }
@@ -34,6 +39,9 @@ module.exports =
 
 
 
+    // module.exports = function(ctx, cb) {
+    //   cb(null, { hire: ctx.data.name || 'Ron Dunphy' });
+    // };
 
     //This is an example from Auth0 that I am setting up.
     // var request = require('request');
