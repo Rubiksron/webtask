428bea5ef8351019a2e2cbce1dc73fec5063a8d1
Rubiksron <rubiksron@gmail.com>
2017-06-24 16:14:59 -0700

    adding a view to foo.js.

--- foo.js
+++ foo.js
@@ -1,6 +1,18 @@
 'use strict';
 var request = require('request');
 
+var view = (function view() {/*
+    <html>
+    <head>
+      <title>Welcome to Webtasks</title>
+    </head>
+    <body>
+      <h1>Hello, <%= name %></h1>
+    </body>
+    </html>
+*/}).toString().match(/[^]*\/\*([^]*)\*\/\s*\}$/)[1];
+
+
 module.exports =
     function (cb) {
         var start = Date.now();
