90b5b6471dbffb5c27479434b88ce8dff9aa39d5
Rubiksron <rubiksron@gmail.com>
2017-06-24 16:05:08 -0700

    deleted unneeded code.

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
 'use strict';
-var view = (function view() {/*
-    <html>
-    <head>
-      <title>Welcome to Webtasks</title>
-    </head>
-    <body>
-      <h1>Hello, <%= name %></h1>
-    </body>
-    </html>
-*/}).toString().match(/[^]*\/\*([^]*)\*\/\s*\}$/)[1];
-
-
 var request = require('request');
 
 module.exports =
