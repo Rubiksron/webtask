9a8ae33656d69d329ba6b24b4ad1c8bc59e89c26
Rubiksron <rubiksron@gmail.com>
2017-06-24 14:38:33 -0700

    trying api with body.conditions property set.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var request = require('request');
 module.exports =
     function (cb) {
         var start = Date.now();
-        request.get('http://api.wunderground.com/api/cc8285fae724e71a/conditions/q/CA/San_Francisco.json', function (error, res, body) {
+        request.get('http://api.wunderground.com/api/cc8285fae724e71a/conditions/q/WA/Seattle.json', function (error, res, body) {
             if (error)
                 cb(error);
             else
@@ -24,24 +24,17 @@ module.exports =
                     status: res.statusCode,
                     length: body.length,
                     latency: Date.now() - start,
-                    weather:  [
-                      {
-                        id: 800,
-                        main: "Clear",
-                        description: "clear sky",
-                        icon: "02n"
-                      }
-                    ]
+                    body: body.conditions
                 });
         });
     }
 
 
 
-
     // module.exports = function(ctx, cb) {
     //   cb(null, { hire: ctx.data.name || 'Ron Dunphy' });
     // };
+
 
     //This is an example from Auth0 that I am setting up.
     // var request = require('request');
