9987243acb0a6975a65c56ce0c99f1a04755c8ce
Rubiksron <rubiksron@gmail.com>
2017-06-24 15:51:00 -0700

    back to WU api.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var request = require('request');
 module.exports =
     function (cb) {
         var start = Date.now();
-        request.get('http://wttr.in/seattle', function (error, res, body) {
+        request.get('http://api.wunderground.com/api/cc8285fae724e71a/conditions/q/WA/Seattle.json', function (error, res, body) {
             if (error)
                 cb(error);
             else
