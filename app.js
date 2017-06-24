47a00c84cd967d053e000eb243b6dbc2e98d9e48
Rubiksron <rubiksron@gmail.com>
2017-06-24 15:48:23 -0700

    trying wttr.in/seattle api.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var request = require('request');
 module.exports =
     function (cb) {
         var start = Date.now();
-        request.get('http://api.wunderground.com/api/cc8285fae724e71a/conditions/q/WA/Seattle.json', function (error, res, body) {
+        request.get('http://wttr.in/seattle', function (error, res, body) {
             if (error)
                 cb(error);
             else
@@ -24,7 +24,8 @@ module.exports =
                     status: res.statusCode,
                     length: body.length,
                     latency: Date.now() - start,
-                    response: res.body
+                    response: res.body,
+
                 });
         });
     }
