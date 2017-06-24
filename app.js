51d6771390cc73c99b419287a189c465f95d2ea7
Rubiksron <rubiksron@gmail.com>
2017-06-24 13:44:07 -0700

    checking different end point.

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 'use strict';
 //This is an example from Auth0 that I am setting up.
+// var request = require('request');
+//
+// module.exports =
+//     function (cb) {
+//         var start = Date.now();
+//         request.get('https://auth0.com', function (error, res, body) {
+//             if (error)
+//                 cb(error);
+//             else
+//                 cb(null, {
+//                     status: res.statusCode,
+//                     length: body.length,
+//                     latency: Date.now() - start
+//                 });
+//         });
+//     }
+
 var request = require('request');
 
 module.exports =
     function (cb) {
         var start = Date.now();
-        request.get('https://auth0.com', function (error, res, body) {
+        request.get('https://google.com', function (error, res, body) {
             if (error)
                 cb(error);
             else
