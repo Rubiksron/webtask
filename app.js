2db9bf6122608e491683743b93c2b3c5b18b60b9
Rubiksron <rubiksron@gmail.com>
2017-06-24 14:08:27 -0700

    adding api

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
 'use strict';
-//This is an example from Auth0 that I am setting up.
-// var request = require('request');
-//
-// module.exports =
-//     function (cb) {
-//         var start = Date.now();
-//         request.get('https://auth0.com', function (error, res, body) {
-//             if (error)
-//                 cb(error);
-//             else
-//                 cb(null, {
-//                     status: res.statusCode,
-//                     length: body.length,
-//                     latency: Date.now() - start
-//                 });
-//         });
-//     }
+var view = (function view() {/*
+    <html>
+    <head>
+      <title>Welcome to Webtasks</title>
+    </head>
+    <body>
+      <h1>Hello, <%= name %></h1>
+    </body>
+    </html>
+*/}).toString().match(/[^]*\/\*([^]*)\*\/\s*\}$/)[1];
+
+module.exports = function(ctx, cb) {
+  cb(null, { hire: ctx.data.name || 'Ron Dunphy' });
+};
 
 var request = require('request');
 
 module.exports =
     function (cb) {
         var start = Date.now();
-        request.get('https://google.com', function (error, res, body) {
+        request.get('http://api.wunderground.com/api/Your_Key/conditions/q/CA/Seattle.json', function (error, res, body) {
             if (error)
                 cb(error);
             else
@@ -33,3 +30,25 @@ module.exports =
                 });
         });
     }
+
+
+
+
+
+    //This is an example from Auth0 that I am setting up.
+    // var request = require('request');
+    //
+    // module.exports =
+    //     function (cb) {
+    //         var start = Date.now();
+    //         request.get('https://auth0.com', function (error, res, body) {
+    //             if (error)
+    //                 cb(error);
+    //             else
+    //                 cb(null, {
+    //                     status: res.statusCode,
+    //                     length: body.length,
+    //                     latency: Date.now() - start
+    //                 });
+    //         });
+    //     }
