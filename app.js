e8636b451e1fb5b0401a5818873907fe6c0d6c8e
Rubiksron <rubiksron@gmail.com>
2017-06-24 14:43:16 -0700

    respone object set.

--- app.js
+++ app.js
@@ -24,32 +24,7 @@ module.exports =
                     status: res.statusCode,
                     length: body.length,
                     latency: Date.now() - start,
-                    body: body.conditions
+                    response: {}
                 });
         });
     }
-
-
-
-    // module.exports = function(ctx, cb) {
-    //   cb(null, { hire: ctx.data.name || 'Ron Dunphy' });
-    // };
-
-
-    //This is an example from Auth0 that I am setting up.
-    // var request = require('request');
-    //
-    // module.exports =
-    //     function (cb) {
-    //         var start = Date.now();
-    //         request.get('https://auth0.com', function (error, res, body) {
-    //             if (error)
-    //                 cb(error);
-    //             else
-    //                 cb(null, {
-    //                     status: res.statusCode,
-    //                     length: body.length,
-    //                     latency: Date.now() - start
-    //                 });
-    //         });
-    //     }
