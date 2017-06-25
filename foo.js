5ac3201814895c22927c049e3362708386ace22b
Rubiksron <rubiksron@gmail.com>
2017-06-24 19:50:05 -0700

    blah, blah

--- foo.js
+++ foo.js
@@ -2,18 +2,6 @@
 const request = require('request');
 const json = require('body-parser').json;
 
-let view = (function view() {/*
-    <html>
-    <head>
-      <title>Welcome to Webtasks</title>
-    </head>
-    <body>
-      <h1>Hello</h1>
-    </body>
-    </html>
-*/}).toString().match(/[^]*\/\*([^]*)\*\/\s*\}$/)[1];
-
-
 module.exports =
     function (callback) {
         request.get('http://wttr.in/seattle', function (error, res, body) {
