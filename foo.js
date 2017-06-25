4c492093c8a973caf818d1e02cb23974d1237235
Rubiksron <rubiksron@gmail.com>
2017-06-25 06:47:36 -0700

    diff api

--- foo.js
+++ foo.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const request = require('request');
-let api = 'http://api.worldweatheronline.com/premium/v1/weather.ashx?key=b196ed93029a432dab6121920172506&q=48.85,2.35&num_of_days=2&tp=3&format=json';
+let api = 'http://api.wunderground.com/api/cc8285fae724e71a/conditions/q/CA/Seattle.json';
 module.exports = function (callback) {
         request.get(api, function (error, response, body) {
             if (error)
