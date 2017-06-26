9467917c3d86669bfbf9b8e6ce9255210b01eede
Rubiksron <rubiksron@gmail.com>
2017-06-26 09:26:19 -0700

    reverting back before the view attempt

--- app.js
+++ app.js
@@ -4,18 +4,6 @@ const request = require('request');
 
 const API_KEY = 'http://api.wunderground.com/api/cc8285fae724e71a/conditions/q/WA/Seattle.json';
 
-var view = (function view() {/*
-    <html>
-    <head>
-      <title>Welcome to Webtasks</title>
-    </head>
-    <body>
-      <h1>Hello, <%= name %></h1>
-    </body>
-    </html>
-*/}).toString().match(/[^]*\/\*([^]*)\*\/\s*\}$/)[1];
-
-
 module.exports = function (callback) {
         request.get(API_KEY, function (error, response, body) {
             if (error)
