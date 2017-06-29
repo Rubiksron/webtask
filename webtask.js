1ff6106d50280bb924464fb605a29d9cc8516f3f
Rubiksron <rubiksron@gmail.com>
2017-06-28 22:46:34 -0700

    message unneeded

--- webtask.js
+++ webtask.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var view = (function view() {/*
-    <html>
-    <head>
-      <title>Welcome to Webtasks</title>
-    </head>
-    <body>
-      <h1>Hello, <%= name %></h1>
-    </body>
-    </html>
-*/}).toString().match(/[^]*\/\*([^]*)\*\/\s*\}$/)[1];
+// var view = (function view() {/*
+//     <html>
+//     <head>
+//       <title>Welcome to Webtasks</title>
+//     </head>
+//     <body>
+//       <h1>Hello, <%= name %></h1>
+//     </body>
+//     </html>
+// */}).toString().match(/[^]*\/\*([^]*)\*\/\s*\}$/)[1];
 
 module.exports = function(ctx, cb) {
   cb(null, { hire: ctx.data.name || 'Ron Dunphy' });
